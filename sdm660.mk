f4d794e03f6b9641b4626c6f654cd9323e77c50d
pix106 <sbordenave@gmail.com>
2022-09-20 07:35:27 +0200

    Revert "sdm660-common: Add (unused) qti mapper@2.0 interface libs for blobs"
    This reverts commit a6bd8068c1f4d853a12c5976a116a2a3295f10b6.

--- sdm660.mk
+++ sdm660.mk
@@ -181,7 +181,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
